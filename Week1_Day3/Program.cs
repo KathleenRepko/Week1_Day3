ce66decce7a69f7c6258ce7ce66b42f882f6bb20
KathleenRepko <kathleen.repko@gmail.com>
2017-09-27 14:59:31 -0400

    Dinner bill exercise

--- Week1_Day3/Program.cs
+++ Week1_Day3/Program.cs
@@ -70,33 +70,82 @@ namespace Week1_Day3
             //    Console.WriteLine("That's not really your age.");
             //}
 
-            Console.WriteLine("How many points did you earn?");
-            int pointsEarned = int.Parse(Console.ReadLine());
+            //Console.WriteLine("How many points did you earn?");
+            //int pointsEarned = int.Parse(Console.ReadLine());
 
-            switch (pointsEarned)
+            //switch (pointsEarned)
+            //{
+            //    case 0:
+            //        Console.WriteLine("You lose.");
+            //        break;
+            //    case 1:
+            //        Console.WriteLine("Needs practice.");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("On target.");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Superstar.");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Overachiever.");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("You cheated.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid score entered.");
+            //        break;
+            //}
+
+            //Kathleen's solution:
+            //Console.WriteLine("Each of you will pay $13.00.");
+            //Console.WriteLine("How many guests are in your party?");
+            //int dinerCount = int.Parse(Console.ReadLine());
+
+            //int dinerPayment = dinerCount * 13;
+            //Console.WriteLine("Your total bill is $" + dinerPayment + ".");
+
+            ////Daniel's solution:
+            //int billPortion = 13;
+            //int dinnerNumber = 4;
+            //int billTotal = billPortion * dinnerNumber;
+            //Console.WriteLine(billTotal);
+
+            //Returning to the above problem,replace
+            //hard-coded values of 4 (# of diners) and
+            //13(cost per diner) with vaulues proficed by the user.
+
+            Console.WriteLine("How much will each of you pay?");
+            int dinerPay = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many guests are in your party?");
+            int dinerCount = int.Parse(Console.ReadLine());
+
+            int dinerPayment = dinerPay * dinerCount;
+            Console.WriteLine("Your total bill is $" + dinerPayment + ".");
+
+            //Customer Appreciation Week - $50+ = 10% discount; otherwise 5% discount
+
+            Console.WriteLine("It's Customer Appreciation Week!");
+
+            double newBill;
+
+            if (dinerPayment >= 50)
             {
-                case 0:
-                    Console.WriteLine("You lose.");
-                    break;
-                case 1:
-                    Console.WriteLine("Needs practice.");
-                    break;
-                case 2:
-                    Console.WriteLine("On target.");
-                    break;
-                case 3:
-                    Console.WriteLine("Superstar.");
-                    break;
-                case 4:
-                    Console.WriteLine("Overachiever.");
-                    break;
-                case 5:
-                    Console.WriteLine("You cheated.");
-                    break;
-                default:
-                    Console.WriteLine("Invalid score entered.");
-                    break;
+                newBill = dinerPayment * 0.9;
+                Console.WriteLine("Your discounted total is $" + newBill + ".");
             }
+
+           else
+            {
+                newBill = dinerPayment * 0.95;
+                Console.WriteLine("Your discounted total bill is $" + newBill + "."); 
+            }
+            
+
+         
+         
 
 
 
